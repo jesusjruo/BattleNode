8057fd31d2e7d8bf0f5aeef329366d5c09419d1b
Jesus Urdaneta <jesusjruo@gmail.com>
2019-03-07 14:00:26 -0400

    Agregando chat y condiguraciones iniciales del server

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Entities = require('html-entities').AllHtmlEntities;
+var entities = new Entities();
 
+//var BattleshipGame = ('.app/game.js');
+//var GameStatus = require('./app/gameStatus.js');
+
+var users = {};
+
+var port = 3000;
 
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-      });
-  });
+http.listen(port, function(){
+  console.log('listening on port: ' +port);
+});
+
+// Eventos que ocurren al iniciar un puerto
 
 io.on('connection' , function(socket){
-  socket.on('chat message' , function(msg){
-    console.log('message:' + msg);
-  })
-})
+  console.log((new Date().toISOString()) + 'El jugador ' + socket.id + 'se ha conectado.');
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+  //Se crea el objeto users
+  users[socket.id] ={
+    player: null,
+    inGame: null
+  };
+
+  //Se une a la sala de espera hasta que un segundo jugador inicie el puerto
+  socket.join('Sala de espera');
+
+  //Chat entre jugadores
+  socket.on('chat' , function(msg){
+    if(users[socket.id].inGame != null) {
+      console.log((new Date().toISOString()) + 'Mensaje de ' + socket.id + ':' +msg);
+      socket.broadcast.to(' game ' + user[socket.id].inGame.id).emit('chat',{
+        name: 'Opoenente',
+        message: entities.encode(msg),
+      });
+      io.to(socket.id).emit('chat', {
+        name: 'Yo',
+        message: entities.encode(msg),
+      });
+    }
+  }); 
+
+
 });
